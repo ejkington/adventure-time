5d65ce107f3954f075a7416f8041234a8f0f97cb
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2021-09-28 18:55:36 +0000

    add function too ask players to start game and checks the input and validates it too give the right respons

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Start of game asking players if they want too play.
+if no the game ends with string printed 
+(Thats too bad, hope too see you another day.)
+if yes players is asked to state their name and the game continues.
+if the input is not yes or no, 
+player is asked too try again and returned and asked the question again
+"""
+
+
+def start():
+    print("Hello and welcome to Morbidus castle ")
+    print("Are you ready too play? please use yes or no ")
+    
+    answer_1 = input(">").lower()
+   
+    if "y" in answer_1:
+        print("Great now please state you name. ")
+        name = input()
+        print("Hello " + name)
+    
+    elif "n" in answer_1:
+        print("Thats too bad, hope too see you another day.")
+        
+    else:
+        print("Wrong input try again! ")
+        return start()
+
+
+"""
+Calls the start function
+"""
+
+start()
