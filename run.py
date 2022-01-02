dcff335659677370d417fbf81c6c084b28ff3b04
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-01-02 17:44:26 +0000

    add line breaks

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def start():
     answer_1_1 = input(">").lower().strip()
 
     if "yes" in answer_1_1:
-        print("Great now please state you name.")
+        print("Great now please state you name.\n")
         name = input()
         time.sleep(2)
         print("Hello\n" + name)
@@ -34,7 +34,7 @@ def start():
         prison()
 
     elif "no" in answer_1_1:
-        print("Thats too bad, hope too see you another day.")
+        print("Thats too bad, hope too see you another day.\n")
 
     else:
         print(REQUIRED)
@@ -51,19 +51,19 @@ if wrong input player is sent back to start of prison 1
 
 
 def prison():
-    print("You wake up in a dark room your hands and feets are tied to a bed")
+    print("You wake up in a dark room your hands and feets are tied to a bed \n")
     time.sleep(2)
-    print("you dont remember how you got there")
+    print("you dont remember how you got there \n")
     time.sleep(2)
-    print("you hear sounds of foot steps comming closer.")
+    print("you hear sounds of foot steps comming closer. \n")
     time.sleep(2)
-    print("What do you do?")
+    print("What do you do? \n")
     time.sleep(2)
-    print("Try too struggle too get your hands free.")
+    print("Try too struggle too get your hands free. \n")
     time.sleep(2)
-    print("Lay still and wait.")
+    print("Lay still and wait. \n")
     time.sleep(2)
-    print("Wait or Struggle?")
+    print("Wait or Struggle? \n")
     time.sleep(2)
     prison2()
 
@@ -73,7 +73,7 @@ def prison():
 
 def prison2():
 
-    answer_2_1 = input(">").lower().strip()
+    answer_2_1 = input(">\n").lower().strip()
 
     # checks for players input in second scene
     # if wrong input player gets sent
@@ -81,46 +81,46 @@ def prison2():
 
     if "struggle" in answer_2_1:
         time.sleep(2)
-        print("You take all the streangth you have in your body to try too get lose")
+        print("You take all the streangth you have in your body to try too get lose \n")
         time.sleep(2)
-        print("After a short struggle you get your hands free and can get your feets free")
+        print("After a short struggle you get your hands free and can get your feets free \n")
         time.sleep(2)
-        print("They sounds getting closer and the door opens")
+        print("They sounds getting closer and the door opens\n")
         time.sleep(2)
-        print("What do you do next?")
+        print("What do you do next? \n")
         time.sleep(2)
-        print("Hide or attack?")
+        print("Hide or attack? \n")
         time.sleep(2)
         prison3()
 
     elif "wait" in answer_2_1:
         time.sleep(2)
-        print("You here the footsteps get to the room, you hear keys")
+        print("You here the footsteps get to the room, you hear keys \n")
         time.sleep(2)
-        print("the doors opens a man enters the room")
+        print("the doors opens a man enters the room \n")
         time.sleep(2)
-        print("the man goes up to you, hes clothes is covered in blood")
+        print("the man goes up to you, hes clothes is covered in blood \n")
         time.sleep(2)
-        print("he takes a hold of your arm")
+        print("he takes a hold of your arm \n")
         time.sleep(2)
-        print("What do you do?")
+        print("What do you do? \n")
         time.sleep(2)
-        print("try to run away")
+        print("try to run away \n")
         time.sleep(2)
-        print("attack him")
+        print("attack him \n")
         time.sleep(2)
-        print("Attack or run")
+        print("Attack or run \n")
         prison4()
 
     else:
         print(REQUIRED)
-        print("wait or struggle?")
+        print("wait or struggle? \n")
         prison2()
 
 
 def prison4():
 
-    answer_2_3 = input(">").lower().strip()
+    answer_2_3 = input("> \n").lower().strip()
 
     if "attack" in answer_2_3:
         fight1()
@@ -131,25 +131,25 @@ def prison4():
     else:
         print(REQUIRED)
         time.sleep(2)
-        print("attack or run")
+        print("attack or run \n")
         prison4()
 
 
 def runaway():
-    print("The door is open and you make a run for it")
+    print("The door is open and you make a run for it \n")
     time.sleep(2)
-    print("you get out and run down a long hallway its dark and you can barley see")
+    print("you get out and run down a long hallway its dark and you can barley see \n")
     time.sleep(2)
-    print("the end of the hallway is a dead end but you can feel a sharp object leaning against the wall. ")
+    print("the end of the hallway is a dead end but you can feel a sharp object leaning against the wall. \n")
     time.sleep(2)
-    print("the man in running towards you")
+    print("the man in running towards you \n")
     time.sleep(2)
     time.sleep(2)
-    print("by the feel of it you think the object is a sword")
+    print("by the feel of it you think the object is a sword \n")
     time.sleep(2)
-    print("do you grab the sword ?")
+    print("do you grab the sword ? \n")
     time.sleep(2)
-    print("Yes / No")
+    print("Yes / No \n")
     choice()
 
 # third scene prison players is asked to attack or to hide
@@ -158,45 +158,45 @@ def runaway():
 
 def prison3():
 
-    answer_2_2 = input(">").lower().strip()
+    answer_2_2 = input("> \n").lower().strip()
 
     if "attack" in answer_2_2:
         fight1()
 
     elif "hide" in answer_2_2:
-        print("The man is sees you hiding he takes out a butchers knife")
+        print("The man is sees you hiding he takes out a butchers knife \n")
         time.sleep(2)
-        print("and chops of your head.")
+        print("and chops of your head. \n")
         time.sleep(2)
-        print("You die instantly and with no pain.")
+        print("You die instantly and with no pain. \n")
         time.sleep(2)
-        print("would you like to play again?")
+        print("would you like to play again? \n")
         time.sleep(2)
-        print("Yes / No")
+        print("Yes / No \n")
         time.sleep(2)
         play_again1()
 
     else:
         print(REQUIRED)
-        print("Hide or Attack?")
+        print("Hide or Attack? \n")
         prison3()
 
 
 def fight1():
     time.sleep(2)
-    print("You attack the man, the man turns too you and you are now in a clinch")
+    print("You attack the man, the man turns too you and you are now in a clinch \n")
     time.sleep(2)
-    print("the man reaches for his butcher knife")
+    print("the man reaches for his butcher knife \n")
     time.sleep(2)
-    print("you grab his hand to try to stop him")
+    print("you grab his hand to try to stop him \n")
     time.sleep(2)
-    print("the man is stronger than you and pushes you of")
+    print("the man is stronger than you and pushes you of \n")
     time.sleep(2)
-    print("you fall backwards in the corner of the room on the floor there is a small sword")
+    print("you fall backwards in the corner of the room on the floor there is a small sword \n")
     time.sleep(2)
-    print("do you grab the sword ?")
+    print("do you grab the sword ? \n")
     time.sleep(2)
-    print("Yes / No")
+    print("Yes / No \n")
     choice()
 
     """
@@ -205,7 +205,7 @@ def fight1():
 
 
 def choice():
-    answer_3_1 = input(">").lower().strip()
+    answer_3_1 = input("> \n").lower().strip()
     time.sleep(2)
 
     # player choose to pick up sword of leave it.
@@ -218,27 +218,27 @@ def choice():
 
     else:
         print(REQUIRED)
-        print("Yes / No")
+        print("Yes / No \n")
         choice()
 
     if SWORD > 0:
-        print("You grab the sword and point it at the man")
+        print("You grab the sword and point it at the man \n")
         time.sleep(2)
-        print("the man runs in full force and the sword pierces him")
+        print("the man runs in full force and the sword pierces him \n")
         time.sleep(2)
-        print("the man falls down on the ground and screams")
+        print("the man falls down on the ground and screams \n")
         time.sleep(2)
-        print("GET OUT OF HERE!")
+        print("GET OUT OF HERE! \n")
         time.sleep(2)
         win()
     else:
-        print("you dident pick up the sword and are now defenseless")
+        print("you dident pick up the sword and are now defenseless \n")
         time.sleep(2)
-        print("the man makes quit work of you and you...")
+        print("the man makes quit work of you and you... \n")
         time.sleep(2)
-        print("and you...")
+        print("....... \n")
         time.sleep(4)
-        print("DIE")
+        print("DIE \n")
         play_again1()
 
 
@@ -246,17 +246,17 @@ def choice():
 
 
 def play_again1():
-    print("Do you want to play again ?")
+    print("Do you want to play again ? \n")
     time.sleep(1)
-    print("Yes / No")
+    print("Yes / No \n")
 
-    play_again = input(">").lower().strip()
+    play_again = input("> \n").lower().strip()
 
     if "yes" in play_again:
         start()
 
     elif "no" in play_again:
-        print("See you some other time!")
+        print("See you some other time! \n")
 
     else:
         print(REQUIRED)
@@ -267,13 +267,13 @@ def play_again1():
 
 def win():
     time.sleep(2)
-    print("you made it")
+    print("you made it \n")
     time.sleep(2)
-    print("you have escaped the man")
+    print("you have escaped the man \n")
     time.sleep(2)
-    print("for now..........")
+    print("for now.......... \n")
     time.sleep(2)
-    print("TO BE CONTINUED.")
+    print("TO BE CONTINUED. \n")
     play_again1()
 
     """
